0c9ee1ef9565d839c8e4e87fdaa10a9f550de2c4
Andersen <andersenmp@gmail.com>
2014-05-08 15:33:31 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #set -xv
 
-if [ $# -eq 0 ]
-  then
+if [ $# -eq 0 ] then
     echo "Version number is needed!"
     echo "Usage: ./build.sh version"
     exit 0
+fi
+
+if [ ! -d build ]; then
+    mkdir build
+    mkdir build/pipic
 fi
 
 rm -rfv build/pipic/*
@@ -13,11 +17,11 @@ cp -r src/* build/pipic/
 cd build
 rm pipic/*.pyc
 rm pipic/pics/pic_*
-tar -czvf ../bkp/pipic.$1.$(date -d "today" +"%Y%m%d_%H%M").tar.gz pipic
+#tar -czvf ../bkp/pipic.$1.$(date -d "today" +"%Y%m%d_%H%M").tar.gz pipic
 rm pipic/parameters.ini
 cp pipic/parameters.dpl pipic/parameters.ini
 tar -czvf pipic.$1.$(date -d "today" +"%Y%m%d_%H%M").tar.gz pipic
-cp  pipic.$1.$(date -d "today" +"%Y%m%d_%H%M").tar.gz  ~/www/pipic/build/
+#cp  pipic.$1.$(date -d "today" +"%Y%m%d_%H%M").tar.gz  ~/www/pipic/build/
 cd ..
 
 
